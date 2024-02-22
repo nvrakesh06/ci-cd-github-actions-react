61cef80f7067d1de39223a56e1469eeca25673af
hemag2804 <gudahemareddy88@gmail.com>
2024-02-22 21:48:45 +0530

    changes made to App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, Running on github hosted runners
         </a>
       </header>
     </div>
